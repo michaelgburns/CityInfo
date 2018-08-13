29195b29125f1e4922cae78568586541985e026e
michaelgburns <michael.burns@civica.co.uk>
2018-08-13 06:12:40 +0100

    Starting to add new controller and implementing Http get actions

---
+++ CityInfo/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using CityInfo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            return new JsonResult(city);
+        }
+    }
+}
---
+++ CityInfo/Models/CitiesDataStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Models
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>
+            {
+                new CityDto
+                {
+                    Id          = 1,
+                    Name        = "Dublin",
+                    Description = "Dublin city"
+                },
+
+                new CityDto
+                {
+                    Id          = 2,
+                    Name        = "Belfast",
+                    Description = "Belfast city"
+                }
+            };
+        }
+    }
+}

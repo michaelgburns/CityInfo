03214c822b47161b12571fab0614b1638f025de3
michaelgburns <michael.burns@civica.co.uk>
2018-08-17 07:40:30 +0100

    Adding an example of overriding the default Json serialisation

--- CityInfo/Startup.cs
+++ CityInfo/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo
 {
@@ -15,7 +16,17 @@ namespace CityInfo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(o => //This section is just an example of overriding the default Json set up
+                {
+                    if (o.SerializerSettings != null)
+                    {
+                        // The default sets the first character of Json part to lower case.
+                        // The below code just prevents the default behaviour. The exact class names will be used in the Json
+                        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;                    
+                        castedResolver.NamingStrategy = null;
+                    }
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

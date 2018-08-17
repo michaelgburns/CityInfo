1440e471fd057cff05a6cadc99bd4472a8a39c6a
michaelgburns <michael.burns@civica.co.uk>
2018-08-17 08:13:16 +0100

    An example of adding an xml output formatter

--- CityInfo/Startup.cs
+++ CityInfo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace CityInfo
 {
@@ -16,7 +17,7 @@ namespace CityInfo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
+            services.AddMvc().AddMvcOptions(o => o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
                 //.AddJsonOptions(o => //This section is just an example of overriding the default Json set up
                 //{
                 //    if (o.SerializerSettings != null)

4f4ea7f9e49cf8b7ca3081dc7f2d8ddb47a61c0c
michaelgburns <michael.burns@civica.co.uk>
2018-08-17 07:47:35 +0100

    Removing Json formatter example

--- CityInfo/Startup.cs
+++ CityInfo/Startup.cs
@@ -17,16 +17,16 @@ namespace CityInfo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
-                .AddJsonOptions(o => //This section is just an example of overriding the default Json set up
-                {
-                    if (o.SerializerSettings != null)
-                    {
-                        // The default sets the first character of Json part to lower case.
-                        // The below code just prevents the default behaviour. The exact class names will be used in the Json
-                        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;                    
-                        castedResolver.NamingStrategy = null;
-                    }
-                });
+                //.AddJsonOptions(o => //This section is just an example of overriding the default Json set up
+                //{
+                //    if (o.SerializerSettings != null)
+                //    {
+                //        // The default sets the first character of Json part to lower case.
+                //        // The below code just prevents the default behaviour. The exact class names will be used in the Json
+                //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;                    
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

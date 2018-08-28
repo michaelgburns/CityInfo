d31c522b19b31d2a42be7ab9f4d28d3a90efcd9a
michaelgburns <michael.burns@civica.co.uk>
2018-08-28 11:05:42 +0100

    Starting to use the repository over the hard coded data

--- CityInfo/Controllers/CitiesController.cs
+++ CityInfo/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.Models;
+using CityInfo.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,33 @@ namespace CityInfo.Controllers
     [Route("api/cities")]
     public class CitiesController : Controller
     {
+        private ICityInfoRepository _cityInfoRepository;
+
+        public CitiesController(ICityInfoRepository cityInfoRepository)
+        {
+            _cityInfoRepository = cityInfoRepository;
+        }
+
         [HttpGet]
         public IActionResult GetCities()
         {
-            return Ok(CitiesDataStore.Current.Cities);
+            //return Ok(CitiesDataStore.Current.Cities);
+
+            var cityResults = _cityInfoRepository.GetCities();
+
+            var results = new List<CityWithoutPointsOfInterestDto>();
+
+            foreach (var city in cityResults)
+            {
+                results.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id          = city.Id,
+                    Name        = city.Name,
+                    Description = city.Description
+                });
+            }
+
+            return Ok(results);
         }
 
         [HttpGet("{id}")]
